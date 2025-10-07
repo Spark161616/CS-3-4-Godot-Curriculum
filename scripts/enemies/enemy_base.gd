228867adc2393162c5927c053f7833b88d368013
Spark161616 <fmills6387@student.pps.net>
2025-10-07 14:41:36 -0700

    t

--- scripts/enemies/enemy_base.gd
+++ scripts/enemies/enemy_base.gd
@@ -13,14 +13,17 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	super._physics_process(delta)
+	print(position)
 	pass
-
+	
 
 func _on_detection_radius_body_entered(body: Node2D) -> void:
 	super._on_detection_radius_body_entered(body)
-	pass # Replace with function body.
+	if body is Player:
+		is_hostile=true
 
 
 func _on_detection_radius_body_exited(body: Node2D) -> void:
 	super._on_detection_radius_body_exited(body)
-	pass # Replace with function body.
+	if body is Player:
+		is_hostile=false
