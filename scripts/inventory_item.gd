5db9c26b64281a765fbb89d6029c7ef369521471
Pace Lykins <plykins4127@student.pps.net>
2025-10-24 15:30:33 -0700

    Update inventory_item.gd

--- scripts/inventory_item.gd
+++ scripts/inventory_item.gd
@@ -1,6 +1,13 @@
 extends Resource
+class_name inventory_item
 
 @export var name : String
 @export var description : String
 @export var cost : int
 @export var color : Color
+
+func _ready():
+	pass
+	
+func _process():
+	pass
