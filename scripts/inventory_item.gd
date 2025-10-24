571eba45e56694e86e13df3c88da3d35171a3fe5
Spark161616 <fmills6387@student.pps.net>
2025-10-24 15:32:38 -0700

    Update inventory_item.gd

--- scripts/inventory_item.gd
+++ scripts/inventory_item.gd
@@ -1,6 +1,10 @@
 extends Resource
+class_name inventory_item
 
-@export var name : String
-@export var description : String
+@export var descrption : String
 @export var cost : int
-@export var color : Color
+
+func _ready():
+		pass
+func _process():
+	pass
